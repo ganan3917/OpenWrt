17161844a60038b512849798d34b718f87bfaf99
ganan3917 <134137519+ganan3917@users.noreply.github.com>
2024-10-26 23:20:21 +0800

    Update diy-pass.sh

--- diy-pass.sh
+++ diy-pass.sh
@@ -32,7 +32,8 @@ function git_sparse_clone() {
 # git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
-
+ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ 
 # 科学上网插件
 # git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
